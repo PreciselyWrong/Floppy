17668a571d4c7998406a39ce649496970d46f824
Sni3rs <coppens.n+gitea@gmail.com>
2026-08-24 16:41:51 +0200

    chore: merge custom user migration leaves
    - join Glass Cinema with the existing Home integration graph
    - keep contribution migrations independent upstream

---
+++ src/users/migrations/0131_merge_glass_theme_home.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("users", "0129_user_glass_theme"),
+        ("users", "0130_merge_user_appearance_home"),
+    ]
+
+    operations = []
