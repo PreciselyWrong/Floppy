84c40386ffce04fa8a0a7a083a526843c5a364b4
Sni3rs <coppens.n+gitea@gmail.com>
2026-08-26 16:18:47 +0200

    chore: prepare media history deployment
    - merge the custom migration leaves
    - rebuild Tailwind against the combined custom branch
    - register the validated Home and History fixes

---
+++ src/users/migrations/0141_merge_home_media_type_chip_preferences.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("users", "0131_user_home_media_type_chip_preferences"),
+        ("users", "0140_remove_homescreenrow_home_screen_row_row_type_valid_and_more"),
+    ]
+
+    operations = []
